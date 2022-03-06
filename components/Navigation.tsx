365aeb158db63887a07255d1af0982626d547d8b
Jakub Siemienowicz <jacobsiemienowicz@gmail.com>
2022-03-06 10:29:48 +0100

    Created sklep category

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -131,7 +131,7 @@ function Navigation() {
               E-Learning
             </a>
             <NavLink
-              href="/shop"
+              href="/sklep"
               className="text-base font-medium text-gray-500 hover:text-gray-900"
               activeClassName="text-gray-900 hover:text-gray-700 underline underline-offset-2"
             >
@@ -204,7 +204,7 @@ function Navigation() {
                   >
                     E-Learning
                   </a>
-                  <Link href="/shop">
+                  <Link href="/sklep">
                     <a className="text-base font-medium text-gray-900 hover:text-gray-700">
                       Oprogramowanie
                     </a>
